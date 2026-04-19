0979451115afd773fbf7459f6a9b8c768c1b2990
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-04-19 22:04:01 +0600

    exploring HeroUi component library just like DaisyUi

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@heroui/react";
+
+const HeroPage = () => {
+    return (
+        <div>
+            <h2>Hero Page</h2>
+            <Button>Click Me</Button>
+            <Button variant="outline">Click Me</Button>
+            <Button variant="secondary">Click Me</Button>
+            <Button variant="danger">Click Me</Button>
+        </div>
+    );
+};
+
+export default HeroPage;
