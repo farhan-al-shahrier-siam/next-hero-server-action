8fc698c6b9987f61e642c4244a0bc4f91bc2cd6d
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-04-20 17:52:28 +0600

    Exploring form from heroUi and collecting the data from the form

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,4 +1,6 @@
+import AddTask from "@/components/AddTask";
 import TasksCard from "@/components/TasksCard";
+import { createATask } from "@/lib/actions";
 import { getTask } from "@/lib/tasks";
 
 const TasksPage = async () => {
@@ -9,6 +11,9 @@ const TasksPage = async () => {
     return (
         <div>
             <h2>Task: {tasks.length}</h2>
+            <div className="my-4">
+                <AddTask createATask={createATask}></AddTask>
+            </div>
             <div className="grid grid-cols-3 gap-4">
                 {
                     tasks.map(task => <TasksCard task={task} key={task.id}></TasksCard>)
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,100 @@
+"use client";
+import { CirclePlus } from "@gravity-ui/icons";
+import { Button, Input, Label, Modal, Surface, TextField, ListBox, Select } from "@heroui/react";
+
+const AddTask = ({ createATask }) => {
+    return (
+        <div>
+            <Modal>
+                <Button variant="secondary">Add a Task</Button>
+                <Modal.Backdrop>
+                    <Modal.Container placement="auto">
+                        <Modal.Dialog className="sm:max-w-md">
+                            <Modal.CloseTrigger />
+                            <Modal.Header>
+                                <Modal.Icon className="bg-accent-soft text-accent-soft-foreground">
+                                    <CirclePlus className="size-5" />
+                                </Modal.Icon>
+                                <Modal.Heading>Add a Task</Modal.Heading>
+                            </Modal.Header>
+                            <Modal.Body className="p-6">
+                                <Surface variant="default">
+                                    <form action={createATask} className="flex flex-col gap-4">
+                                        <TextField className="w-full" name="title" type="text">
+                                            <Label>Title</Label>
+                                            <Input placeholder="Enter your task title" />
+                                        </TextField>
+                                        <TextField className="w-full" name="description" type="text">
+                                            <Label>Description</Label>
+                                            <Input placeholder="Enter task description" />
+                                        </TextField>
+                                        <Select name="prority" className="w-[256px]" placeholder="Select one">
+                                            <Label>Prority</Label>
+                                            <Select.Trigger>
+                                                <Select.Value />
+                                                <Select.Indicator />
+                                            </Select.Trigger>
+                                            <Select.Popover>
+                                                <ListBox>
+                                                    <ListBox.Item id="low" textValue="Low">
+                                                        Low
+                                                        <ListBox.ItemIndicator />
+                                                    </ListBox.Item>
+                                                    <ListBox.Item id="medium" textValue="Medium">
+                                                        Medium
+                                                        <ListBox.ItemIndicator />
+                                                    </ListBox.Item>
+                                                    <ListBox.Item id="high" textValue="High">
+                                                        High
+                                                        <ListBox.ItemIndicator />
+                                                    </ListBox.Item>
+                                                </ListBox>
+                                            </Select.Popover>
+                                        </Select>
+
+                                        <Select name="status" className="w-[256px]" placeholder="Select one">
+                                            <Label>Status</Label>
+                                            <Select.Trigger>
+                                                <Select.Value />
+                                                <Select.Indicator />
+                                            </Select.Trigger>
+                                            <Select.Popover>
+                                                <ListBox>
+                                                    <ListBox.Item id="pending" textValue="Pending">
+                                                        Pending
+                                                        <ListBox.ItemIndicator />
+                                                    </ListBox.Item>
+                                                    <ListBox.Item id="inProgress" textValue="In-Progress">
+                                                        In-Progress
+                                                        <ListBox.ItemIndicator />
+                                                    </ListBox.Item>
+                                                    <ListBox.Item id="completed" textValue="Completed">
+                                                        Completed
+                                                        <ListBox.ItemIndicator />
+                                                    </ListBox.Item>
+                                                </ListBox>
+                                            </Select.Popover>
+                                        </Select>
+
+                                        <TextField className="w-full" name="assignedTo">
+                                            <Label>Assigned to:</Label>
+                                            <Input placeholder="Enter assignee name" />
+                                        </TextField>
+                                        <Modal.Footer>
+                                            <Button slot="close" variant="secondary">
+                                                Cancel
+                                            </Button>
+                                            <Button type="submit">Submit task</Button>
+                                        </Modal.Footer>
+                                    </form>
+                                </Surface>
+                            </Modal.Body>
+                        </Modal.Dialog>
+                    </Modal.Container>
+                </Modal.Backdrop>
+            </Modal>
+        </div>
+    );
+};
+
+export default AddTask;
